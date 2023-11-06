3e9c8a4e15892734c73ad01e8b85ee581e6922ff
mesaian <mesaian@saber.games>
2023-11-06 05:48:10 +0300

    Lab 2.02.

--- src/renderer/raytracer/raytracer_renderer.cpp
+++ src/renderer/raytracer/raytracer_renderer.cpp
@@ -31,6 +31,8 @@ void cg::renderer::ray_tracing_renderer::init()
 	raytracer = std::make_shared<cg::renderer::raytracer<cg::vertex, cg::unsigned_color>>();
 	raytracer->set_viewport(settings->width, settings->height);
 	raytracer->set_render_target(render_target);
+	raytracer->set_vertex_buffers(model->get_vertex_buffers());
+	raytracer->set_index_buffers(model->get_index_buffers());
 	// TODO Lab: 2.03 Add light information to `lights` array of `ray_tracing_renderer`
 	// TODO Lab: 2.04 Initialize `shadow_raytracer` in `ray_tracing_renderer`
 }
@@ -46,6 +48,13 @@ void cg::renderer::ray_tracing_renderer::render()
 		payload.color = {0.f, 0.f, (ray.direction.y + 1.f) * 0.5f};
 		return payload;
 	};
+	raytracer->closest_hit_shader = [](const ray& ray, payload& payload,
+									   const triangle<cg::vertex>& triangle,
+									   size_t depth) {
+		payload.color = cg::color::from_float3(triangle.diffuse);
+		return payload;
+	};
+	raytracer->build_acceleration_structure();
 	raytracer->clear_render_target({0, 0, 0});
 
 	auto start = std::chrono::high_resolution_clock::now();
@@ -65,7 +74,6 @@ void cg::renderer::ray_tracing_renderer::render()
 			);
 
 	cg::utils::save_resource(*render_target, settings->result_path);
-	// TODO Lab: 2.02 Add `closest_hit_shader` to `raytracer` class to return diffuse color
 	// TODO Lab: 2.03 Adjust `closest_hit_shader` of `raytracer` to implement Lambertian shading model
 	// TODO Lab: 2.04 Define `any_hit_shader` and `miss_shader` for `shadow_raytracer`
 	// TODO Lab: 2.04 Adjust `closest_hit_shader` of `raytracer` to cast shadows rays and to ignore occluded lights
